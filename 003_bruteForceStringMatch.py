e6f3b2801196d478ad030ccebc5f4ef7367c4509
athena272 <guilhermera272@gmail.com>
2023-01-26 12:03:07 -0300

    add code

---
+++ 003_bruteForceStringMatch.py
@@ -0,0 +1,17 @@
+def findSubstring(text, subString):
+    sizeText = len(text)
+    sizesubString = len(subString)
+    for i in range(0, sizeText - sizesubString):
+        j = 0
+        while j < sizesubString and subString[j] == text[i + j]:
+            j = j + 1
+
+        if j == sizeText:
+            return i
+                
+    return -1
+
+string = "Hello, World!"
+substring = "World"
+
+print(findSubstring(string, substring))
