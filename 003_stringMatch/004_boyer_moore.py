eb1c4d743479847e70d58c28b3b5ac237a3e0745
athena272 <guilhermera272@gmail.com>
2023-02-01 10:40:39 -0300

    Create 004_boyer_moore.py

---
+++ 003_stringMatch/004_boyer_moore.py
@@ -0,0 +1,19 @@
+# The Boyer-Moore algorithm is a string matching algorithm that can be used to efficiently search for a substring within a larger string
+
+# from pyxDamerauLevenshtein import damerau_levenshtein_distance, boyer_moore
+# string = "Hello, World!"
+# substring = "World"
+# index = boyer_moore(string, substring)
+# print(index)
+
+# pyxDamerauLevenshtein is not a python library. Boyer-Moore algorithm can be implemented manually in python, The basic idea behind the algorithm is to preprocess the substring to create a "bad character" rule and a "good suffix" rule, which can be used to quickly skip over parts of the larger string that are unlikely to contain the substring.
+
+# However, python's re library has an implementation of Boyer-Moore algorithm, you can use re.search(pattern, string, flags=re.S) to find the substring in the given string.
+
+# It's also possible to use python's str.find() or str.index() method to find the substring in a given string, but Boyer-Moore algorithm is more efficient when the substring is long and the string is large.
+
+import re
+string = "Hello, World!"
+substring = "World"
+result = re.search(substring, string)
+print(result.start())
