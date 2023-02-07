f2fab36d04a443a9110179b0fbc0558a6ca53ee8
athena272 <guilhermera272@gmail.com>
2023-02-07 14:47:46 -0300

    Update closest_pair_points.py

--- 000_theHuxley/closest_pair_points.py
+++ 000_theHuxley/closest_pair_points.py
@@ -1,3 +1,11 @@
+# Explanation:
+
+# The function dist calculates the Euclidean distance between two points (x1, y1) and (x2, y2).
+# The function closest_pair takes a list of points as input and calculates the minimum distance between any two points using nested for loops.
+# The main loop continues to read the input until it encounters a case with n = 0, which signals the end of the input.
+# For each case, the input points are stored in a list, and the minimum distance between any two points is calculated by calling the closest_pair function.
+# If the minimum distance is greater than or equal to 10000, "INFINITY" is printed. Otherwise, the minimum distance is printed with 4 decimal places.
+
 import math
 
 
