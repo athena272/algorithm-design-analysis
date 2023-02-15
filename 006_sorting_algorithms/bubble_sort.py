ca8b1eb65288a244cbb10742e8073ec33804072f
athena272 <guilhermera272@gmail.com>
2023-02-15 18:06:44 -0300

    Update bubble_sort.py

--- 006_sorting_algorithms/bubble_sort.py
+++ 006_sorting_algorithms/bubble_sort.py
@@ -5,3 +5,6 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
+array = [5, 8, 3, -1, 4, 7];
+print(bubble_sort(array))
